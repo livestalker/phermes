ae520451436b81505b4696c60b32ee2ffa05ed62
LiveStalker <alexey@livestalker.net>
2011-03-28 20:34:14 +0400

    modified:   .gitignore 	modified:   settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,5 @@
 import os.path
 
-import os
-
 # Django settings for pgermes project.
 
 DEBUG = True
@@ -18,12 +16,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql',               # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pgermes',               # Or path to database file if using sqlite3.
-        'USER': 'pgermes',               # Not used with sqlite3.
-        'PASSWORD': 'pgermes',           # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE'    : 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME'      : 'pgermes',                  # Or path to database file if using sqlite3.
+        'USER'      : 'pgermes',                  # Not used with sqlite3.
+        'PASSWORD'  : 'pgermes',                  # Not used with sqlite3.
+        'HOST'      : '',                         # Set to empty string for localhost. Not used with sqlite3.
+        'PORT'      : '',                         # Set to empty string for default. Not used with sqlite3.
     }
 }
 
