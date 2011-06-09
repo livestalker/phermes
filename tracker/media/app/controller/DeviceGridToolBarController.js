f937e005c8eebf4a84c4f10707e43c9123830c23
LiveStalker <alexey@livestalker.net>
2011-06-09 16:39:33 +0400

    modified:   tracker/media/app/controller/DeviceGridToolBarController.js 	modified:   tracker/media/css/tracker.css

--- tracker/media/app/controller/DeviceGridToolBarController.js
+++ tracker/media/app/controller/DeviceGridToolBarController.js
@@ -83,7 +83,7 @@ Ext.define('Tracker.controller.DeviceGridToolBarController', {
      * 
      */
     deleteDevice: function() {
-        
+        this.showMsg('title', 'test');
     },
     /**
      * Save new Device or save changes
@@ -103,5 +103,25 @@ Ext.define('Tracker.controller.DeviceGridToolBarController', {
     cancelSave: function() {
         var win = this.getDeviceWindow();
         win.close();
+    },
+    showMsg: function(title, text, format) {
+        var msgCt = Ext.get('msg-div');
+        if(!msgCt){
+            msgCt = Ext.core.DomHelper.insertFirst(document.body, {
+                id:'msg-div'
+            }, true);
+        }
+        //var s = Ext.String.format.apply(String, Array.prototype.slice.call(arguments, 1));
+        var m = Ext.core.DomHelper.append(msgCt, 
+            '<div class="msg"><h3>' + 
+            title + 
+            '</h3><p>' +
+            text + 
+            '</p></div>', true);
+        m.hide();
+        m.slideIn('t').ghost('t', {
+            delay: 1000, 
+            remove: true
+        });
     }
 });
